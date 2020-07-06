796093dae4db201da38c40533bc0dfb6833c1a96
Harmonised <harmonised7@gmail.com>
2020-07-06 10:04:06 +0300

    1.0 Finished

--- src/main/java/harmonised/imaslug/events/EventHandler.java
+++ src/main/java/harmonised/imaslug/events/EventHandler.java
@@ -5,7 +5,9 @@ import harmonised.imaslug.network.NetworkHandler;
 import harmonised.imaslug.util.ClientHandler;
 import harmonised.imaslug.util.Reference;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Pose;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,14 +15,14 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.*;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class ClientEventHandler
+public class EventHandler
 {
     private static boolean wasCrawling = false, wasVeining = false, wasOpenMenu = false, wasOpenSettings = false, wasOpenSkills = false, wasOpenGlossary = false, tooltipKeyWasPressed = false;
     public static Set<UUID> isCrawling = new HashSet<>();
 
     public static void subscribeClientEvents( IEventBus eventBus )
     {
-        eventBus.register( harmonised.imaslug.events.ClientEventHandler.class );
+        eventBus.register( EventHandler.class );
     }
 
     @SubscribeEvent
@@ -39,5 +41,12 @@ public class ClientEventHandler
             else
                 isCrawling.remove( Minecraft.getInstance().player.getUniqueID() );
         }
+    }
+
+    @SubscribeEvent
+    public static void playerTickEvent( TickEvent.PlayerTickEvent event )
+    {
+        if( isCrawling.contains( event.player.getUniqueID() ) )
+            event.player.setPose( Pose.SWIMMING );
     }
 }
